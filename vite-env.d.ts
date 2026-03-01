023771f78cae0e7562f420d5c7f04f995e6b0df6
Manish <manishkumar9540898@gmail.com>
2026-03-01 15:49:46 +0530

    chore: setup appwrite config

---
+++ vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APPWRITE_API_ENDPOINT: string;
+  readonly VITE_APPWRITE_PROJECT_ID: string;
+  readonly VITE_APPWRITE_DATABASE_ID: string;
+  readonly VITE_APPWRITE_USERS_COLLECTION_ID: string;
+  readonly VITE_APPWRITE_TRIPS_COLLECTION_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
